25fb0260bc184a8157062e9e9d510997cf35579a
retmarutcd <marut@rijwind.be>
2018-09-04 22:37:26 +0200

    forced gitbook results when wanted

--- scripts/gitbooks.sh
+++ scripts/gitbooks.sh
@@ -6,7 +6,11 @@ gitbook epub . sobtec2.epub
 gitbook mobi . sobtec2.mobi
 mv -v *pdf *epub *mobi releases/latest
 git status
-git add -A
+
+echo CTRL-C to NOT push the realses/latest gitbook results to master
+read
+
+git add -A --force releases/latest
 git commit -m "Update gitbook releases with local gitbook script results"
 git push origin master
 echo READY
